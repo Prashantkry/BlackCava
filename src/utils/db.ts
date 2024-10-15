c81cd3e87cd547fde42facdfdca21470c7264373
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-10-15 22:17:36 +0530

    db update with port

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise';
 // Create a connection pool
 const connection = mysql.createPool({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'Asdf@213',
     database: 'blackCava',
